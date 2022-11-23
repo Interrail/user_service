bfb071eae6c4654d2d904c4c2f55c1101b07942d
izzat1998 <izzattilla706@gmail.com>
2022-11-23 14:48:17 +0500

    added sentry

--- app/main.py
+++ app/main.py
@@ -4,6 +4,17 @@ from starlette.middleware.cors import CORSMiddleware
 from app.api.api_v1.api import api_router
 from app.core.config import settings
 
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://0823fcaf4e6e414a98c1a0ac39553903@o4504083668205568.ingest.sentry.io/4504207791357952",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+)
+
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json",
     docs_url=f"{settings.API_V1_STR}/docs")
