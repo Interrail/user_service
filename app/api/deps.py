082210186083934fb39553e4f29bb83d17e718d0
izzat1998 <izzattilla706@gmail.com>
2022-11-23 16:55:25 +0500

    added users async

--- app/api/deps.py
+++ app/api/deps.py
@@ -27,12 +27,11 @@ def get_db() -> Generator:
 async def get_current_user(
         db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)
 ) -> models.User:
-    # try:
-    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[security.ALGORITHM])
-    print(token)
-    token_data = schemas.TokenPayload(**payload)
-    # except (jwt.JWTError, ValidationError):
-    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Could not validate credentials.")
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[security.ALGORITHM])
+        token_data = schemas.TokenPayload(**payload)
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Could not validate credentials.")
     user = crud.user.get(db, id=token_data.sub)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
